87129616655aa06ac7c2a5ef7e9ad76fbd1dd794
zzung <zzeunghee@gmail.com>
2020-09-20 16:46:30 +0900

    admin폴더생성
    management, notice, tip 폴더 생성

--- RecipeChoice/src/com/kh/user/board/controller/BoardDetailViewServlet.java
+++ RecipeChoice/src/com/kh/user/board/controller/BoardDetailViewServlet.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.kh.board.model.service.BoardService;
-import com.kh.board.model.vo.Board;
+import com.kh.user.board.model.service.BoardService;
+import com.kh.user.board.model.vo.Board;
 
 /**
  * Servlet implementation class BoardDetailViewServlet
