418648ff43051236709ea3041f2461abc13e4ec5
SonJiMinn <71236684+SonJiMinn@users.noreply.github.com>
2020-09-29 13:48:02 +0900

    contact

--- RecipeChoice/src/com/kh/admin/report/model/service/ReportService.java
+++ RecipeChoice/src/com/kh/admin/report/model/service/ReportService.java
@@ -1,5 +1,25 @@
 package com.kh.admin.report.model.service;
 
-public class ReportService {
+import static com.kh.user.common.JDBCTemplate.*;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.kh.admin.report.model.dao.ReportDao;
+import com.kh.admin.report.model.vo.Report;
+
+public class ReportService {
+	
+	public ArrayList<Report> selectReportList(){
+		
+		Connection conn = getConnection();
+		
+		ArrayList<Report> list = new ReportDao().selectReportList(conn);
+		
+		close(conn);
+		
+		return list;
+	
+	}
+	
 }
